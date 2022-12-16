fd7b8ae1df655d6b7722092750afdeb93c0b683c
Muhammad Atif <muhammadatifkhan.99@gmail.com>
2022-12-17 01:20:28 +0500

    add some discription to (WritingObjectsToFile)

--- src/WritingObjectsToFile/ObjectExample.java
+++ src/WritingObjectsToFile/ObjectExample.java
@@ -6,8 +6,9 @@ import java.io.ObjectOutputStream;
 public class ObjectExample {
     public static void main(String[] args) throws Exception {
         Student s = new Student("MUhammad","Atif",23);
-
+        //Serialization is done using FileOutputStreams 
         FileOutputStream file = new FileOutputStream("data.ser");
+        //writing object to a file using streams
         ObjectOutputStream out = new ObjectOutputStream(file);
         out.writeObject(s);//this method takes the object of Student class which has implemented the Serializable class
         System.out.println("The operation was successful");
